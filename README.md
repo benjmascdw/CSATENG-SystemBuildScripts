5c23270832c0e975074f741472fa3190d8b9bc4d
Ben Mason <locutus@the-collective.net>
2020-10-19 16:50:58 -0400

    README update

--- README.md
+++ README.md
@@ -17,3 +17,22 @@ cd build\CSATENG-SystemBuildScripts-main
 6. Re-connect via RDP
 7. Run dc1-step2.ps1 (reboot) 
 8. Run dc1-step3.ps1
+
+
+## DC2 Build Steps
+1. Clone Template
+2. Set Password on first boot
+3. Install VMWare tools (Reboot)
+4. Open a powershell prompt and run the following commands to Download and extract the build scripts
+
+'''
+cd Desktop
+(New-Object Net.WebClient).downloadFile("https://github.com/benjmascdw/CSATENG-SystemBuildScripts/archive/main.zip", "c:\Users\Administrator\build.zip")
+Expand-Archive build.zip
+cd build\CSATENG-SystemBuildScripts-main
+'''
+
+5. Run dc2.ps1 (reboot)
+6. Re-connect via RDP
+7. Run dc1-step2.ps1 (reboot) 
+8. Run dc1-step3.ps1
--- dc1-inital.ps1
+++ dc1-inital.ps1
@@ -8,7 +8,7 @@
 $setipaddress=$false
 # $domain_name = "csateng.lab"
 # $hostname = "DC1"
-# $network_interface = "Ethernet0"
+# $network_interface = "Ethernet0"e
 # $ip_Address = "10.89.49.68"
 # $subnet_mask = "26"
 # $default_gateway = "10.89.49.65"
@@ -22,8 +22,8 @@ Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' 
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
 if ($setipaddress -eq $true) {
-    #New-NetIPAddress –InterfaceAlias $network_interface –IPv4Address $ip_Address –PrefixLength $subnet_mask -DefaultGateway $default_gateway
-    #Set-DnsClientServerAddress -InterfaceAlias $network_interface -ServerAddresses $dns_Servers
+    New-NetIPAddress –InterfaceAlias $network_interface –IPv4Address $ip_Address –PrefixLength $subnet_mask -DefaultGateway $default_gateway
+    Set-DnsClientServerAddress -InterfaceAlias $network_interface -ServerAddresses $dns_Servers
 }
 # Set Adapter Category
 Set-NetConnectionProfile -InterfaceAlias $network_interface -NetworkCategory Private
